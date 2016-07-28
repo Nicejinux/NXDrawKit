dbca3aa747f99f9bc942266c8ca414527c97518f
Nicejinux <nicejinux@gmail.com>
2016-07-29 01:55:53 +0900

    test code

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -18,12 +18,4 @@ class Tests: XCTestCase {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
