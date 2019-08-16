49f39dd40f8e32ccfb2e0576959e1c700299d3f9
Niklas Arnitz <niklas.arnitz@jamitlabs.com>
2019-08-16 11:21:28 +0200

    Updated Package.swift to 4.0 syntax

--- Package.swift
+++ Package.swift
@@ -1,9 +1,25 @@
 import PackageDescription
 
 let package = Package(
-  name: "NXDrawKit",
-  dependencies: [],
-  targets: [
-    .target(name: "NXDrawKit", dependencies: []),
-  ]
+    name: "NXDrawKit",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "NXDrawKit",
+            targets: ["NXDrawKit"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target defines a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "NXDrawKit",
+            dependencies: []),
+        .testTarget(
+            name: "NXDrawKitTests",
+            dependencies: ["NXDrawKit"]),
+    ]
 )
