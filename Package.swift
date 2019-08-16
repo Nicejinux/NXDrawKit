233b48aaa06fca1f943433ef185e183e082e49ee
Niklas Arnitz <niklas.arnitz@jamitlabs.com>
2019-08-16 11:28:35 +0200

    modified:   Package.swift

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NXDrawKit",
-            dependencies: []),
-	    path: "NXDrawKit/Classes/",
+            dependencies: [],
+	    path: "NXDrawKit/Classes/"),
         .testTarget(
             name: "NXDrawKitTests",
             dependencies: ["NXDrawKit"]),
